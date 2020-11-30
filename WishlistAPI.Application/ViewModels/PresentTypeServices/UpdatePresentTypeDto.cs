0c89b2f0f9f30adcebdf3bd7be008ed86af11617
Wojtek <wojtek.kajstura@gmail.com>
2020-11-30 17:01:52 +0100

    implemented update present type dto

--- WishlistAPI.Application/ViewModels/PresentTypeServices/UpdatePresentTypeDto.cs
+++ WishlistAPI.Application/ViewModels/PresentTypeServices/UpdatePresentTypeDto.cs
@@ -11,6 +11,12 @@ namespace WishlistAPI.Application.ViewModels.PresentTypeServices
 {
     public class UpdatePresentTypeDto : IMapFrom<PresentType>
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
 
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<UpdatePresentTypeDto, PresentType>();
+        }
     }
 }
