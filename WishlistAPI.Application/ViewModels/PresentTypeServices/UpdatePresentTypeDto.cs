96c7d48b9844d720073573835a4a100601c776d7
Wojtek <wojtek.kajstura@gmail.com>
2020-11-30 17:01:30 +0100

    created update present type dto

---
+++ WishlistAPI.Application/ViewModels/PresentTypeServices/UpdatePresentTypeDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using WishlistAPI.Application.Mapping;
+using WishlistAPI.Domain.Model;
+
+namespace WishlistAPI.Application.ViewModels.PresentTypeServices
+{
+    public class UpdatePresentTypeDto : IMapFrom<PresentType>
+    {
+
+    }
+}
